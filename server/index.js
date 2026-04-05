e0607555583aaf8c03dbcaefeb09147f88bd5ba2
Saifurrahman <saifur13233@gmail.com>
2026-04-05 12:31:12 +0530

    Initial commit, Server is created

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT || 6000;
+
+app.get("/", (req, res) => {
+  return res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
